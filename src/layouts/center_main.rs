a963e2bddd0dc9b3bdcd6b4874067443ff6b1053
Thomas <tdail30@gmail.com>
2021-02-10 11:57:45 -0500

    update center_main to act a bit more like lain centerwork (https://github.com/lcpz/lain)

--- src/layouts/center_main.rs
+++ src/layouts/center_main.rs
@@ -64,13 +64,12 @@ pub fn update(workspace: &Workspace, windows: &mut Vec<&mut &mut Window>) {
     }
 
     let primary_width = match window_count {
-        1 => workspace.width() as i32,
+        // 1 => workspace.width() as i32,
         _ => (workspace.width() as f32 / 2.0).floor() as i32,
     };
 
     let primary_x = match window_count {
-        1 => 0_i32,
-        2 => (workspace.width() as f32 / 2.0).floor() as i32,
+        // 1 => 0_i32,
         _ => (workspace.width() as f32 / 4.0).floor() as i32,
     };
 
@@ -88,7 +87,7 @@ pub fn update(workspace: &Workspace, windows: &mut Vec<&mut &mut Window>) {
 
     let secondary_width = match window_count {
         1 => 0,
-        2 => (workspace.width() as f32 / 2.0).floor() as i32,
+        // 2 => (workspace.width() as f32 / 2.0).floor() as i32,
         _ => (workspace.width() as f32 / 4.0).floor() as i32,
     };
 
